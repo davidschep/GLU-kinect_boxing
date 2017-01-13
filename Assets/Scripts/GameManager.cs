2843becb5739ca9828d59c19edf7574b0701cac4
davidschep <mail@davidschep.com>
2017-01-13 16:06:32 +0100

    Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,140 +1,111 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class GameManager : MonoBehaviour
+namespace BoxDetox
 {
-#region classVariables
-
-    private GameObject[] m_PlayersArray;
-
-    [SerializeField]private UiManager m_UiManager;
-
-    private bool m_bRoundStartOver = false;
-
-    private float m_fRoundTime;
-    private float m_fCoolDownTimer;
-
-    private static int m_iPlayerHealth;
-    private int m_iRounds;
-
-    private int[] m_iPlayerPoints;
-    private int[] m_iPlayerWins;
-
-    public static int Playerhealth
+    public class GameManager : MonoBehaviour
     {
-        get { return m_iPlayerHealth; }
-    }
-    public GameObject[] Players
-    {
-        get { return m_PlayersArray; }
-    }
+        #region classVariables
 
-#endregion
+        private GameObject[] m_PlayersArray;
 
-    private void Start()
-    {
-        m_iRounds = 1;
-        m_fRoundTime = 5f;
-        m_fCoolDownTimer = 5f;
+        [SerializeField]
+        private UiManager m_UiManager;
 
-        m_iPlayerHealth = 100;
+        private static int m_iPlayerHealth;
+        private int m_TotalRounds;
 
-        InitPlayersWithTag("Player");
+        private int[] m_iPlayerPoints;
+        private int[] m_iPlayerWins;
 
-        m_iPlayerWins = new int[m_PlayersArray.Length];
-
-        for (int i = 0; i < m_PlayersArray.Length; i++)
+        public static int Playerhealth
         {
-            m_iPlayerWins[i] = 0;
+            get { return m_iPlayerHealth; }
+        }
+        public GameObject[] Players
+        {
+            get { return m_PlayersArray; }
         }
 
-        PlayerManager.PlayerDowned += IsRoundOver;
-        PlayerManager.PlayerDowned += RestartVariables;
+        private float m_Timer;
+        [SerializeField]
+        private float m_RoundTime = 60f;
+        [SerializeField]
+        private float m_CountDownTime = 5f;
+        public TimerState m_TimerState = TimerState.Waiting;
 
+        #endregion
+
+        private void Start()
+        {
+            m_TotalRounds = 1;
+            m_Timer = m_CountDownTime;
+
+            m_iPlayerHealth = 100;
+
+            InitPlayersWithTag("Player");
+
+            m_iPlayerWins = new int[m_PlayersArray.Length];
+
+            for (int i = 0; i < m_PlayersArray.Length; i++)
+                m_iPlayerWins[i] = 0;
+        }
+
+        private void Update()
+        {
+            m_Timer -= Time.deltaTime;
+            switch (m_TimerState)
+            {
+                case TimerState.Waiting:
+                    m_TimerState = TimerState.CountingDown;
+                    m_Timer = m_CountDownTime;
+                    break;
+                case TimerState.CountingDown:
+                    m_UiManager.m_Centre.text = Mathf.RoundToInt(m_Timer).ToString();
+                    if (m_Timer <= 0)
+                    {
+                        m_TimerState = TimerState.Playing;
+                        m_Timer = m_RoundTime;
+                        m_UiManager.m_Centre.text = "";
+                    }
+                    break;
+                case TimerState.Playing:
+                    m_UiManager.m_TopCentre.text = Mathf.RoundToInt(m_Timer).ToString();
+                    if(m_Timer <= 0)
+                    {
+                        m_TotalRounds++;
+                        m_UiManager.m_TopCentre.text = "Draw!";
+                        m_TimerState = TimerState.Waiting;
+                    }
+                    break;
+            }
+        }
+
+        private void InitPlayersWithTag(string tag)
+        {
+            m_PlayersArray = GameObject.FindGameObjectsWithTag(tag);
+        }
+
+        public void GameWon(int playerWon)
+        {
+            if (playerWon == 0)
+            {
+                m_iPlayerWins[0]++;
+                m_UiManager.m_PlayerWins[0].text = "Wins: " + m_iPlayerWins[0].ToString();
+                m_TimerState = TimerState.Waiting;
+                m_UiManager.m_TopCentre.text = "Player 1 wins!";
+            }
+            else
+            {
+                m_iPlayerWins[1]++;
+                m_UiManager.m_PlayerWins[1].text = "Wins: " + m_iPlayerWins[0].ToString();
+                m_TimerState = TimerState.Waiting;
+                m_UiManager.m_TopCentre.text = "Player 2 wins!";
+            }
+        }
     }
 
-    private void RestartVariables()
+    public enum TimerState
     {
-        m_iRounds = 1;
-        m_fRoundTime = 60f;
-        m_fCoolDownTimer = 5f;
-
-        m_bRoundStartOver = false;
-    }
-
-    private void Update()
-    {
-
-        if (m_bRoundStartOver)
-        {
-            m_fRoundTime -= Time.deltaTime;
-            m_UiManager.m_RoundUIElements[0].text = Mathf.RoundToInt(m_fRoundTime).ToString();
-            m_UiManager.m_RoundUIElements[1].text = "";
-            m_UiManager.m_RoundUIElements[2].text = "Wins: " + m_iPlayerWins[0].ToString();
-
-            //Zet hem aan zodra player 2 er is
-            //m_UiManager.m_RoundUIElements[3].text = "Wins: " + m_iPlayerWins[1].ToString();
-        }
-        else if(!m_bRoundStartOver)
-        {
-            m_fCoolDownTimer -= Time.deltaTime;
-            m_UiManager.m_RoundUIElements[1].text = Mathf.RoundToInt(m_fCoolDownTimer).ToString();
-            if (m_fCoolDownTimer < 0)
-                m_bRoundStartOver = true;
-        }
-
-
-        if (m_fRoundTime < 0)
-        {
-            IsRoundOver();
-            RestartVariables();
-        }
-        //IsRoundOver(isroundover);
-    }
-    /// <summary>
-    /// Puts all the objects with a certain tag in the m_arPlayers array 
-    /// </summary>
-    /// <param name="tag"></param>
-    private void InitPlayersWithTag(string tag)
-    {
-        m_PlayersArray = GameObject.FindGameObjectsWithTag(tag);
-        Debug.Log("Players found: " + m_PlayersArray.Length);
-
-    }
-
-    /// <summary>
-    /// Checks the bool if its true and then executes the function
-    /// </summary>
-    /// <param name="roundover"></param>
-    private void IsRoundOver()
-    {
-
-            bool[] playerdown = GetComponent<PlayerManager>().PlayerDown;
-            RoundCounter(playerdown);
-
-    }
-    private void RoundCounter(bool[] playerdown)
-    {
-        if (playerdown[0])
-        {
-
-            //Player 2 won
-            m_iPlayerWins[1]++;
-            Debug.Log("Player wins: " + m_iPlayerWins[1]);
-            m_UiManager.m_RoundUIElements[3].text = "Wins: " + m_iPlayerWins[0].ToString();
-        }
-        else if(playerdown[1])
-        {
-            //Player 1 won
-            m_iPlayerWins[0] += 1;
-            Debug.Log("Player wins: " + m_iPlayerWins[0]);
-            m_UiManager.m_RoundUIElements[2].text = "Wins: " + m_iPlayerWins[0].ToString();
-        }
-        else
-        {
-            m_UiManager.m_RoundUIElements[2].text = "Draw!";
-            m_UiManager.m_RoundUIElements[3].text = "Draw!";
-        }
-
-    }
+        Playing, CountingDown, Waiting
+    };
 }
