dbba595f5cc5da7acde0401119ca3c357c6fe2da
Storm <stormwitziers123@gmail.com>
2017-01-14 00:13:52 +0100

    Added Main menu
    Added a start menu

---
+++ Assets/Scripts/StartMenuEffects.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartMenuEffects : MonoBehaviour
+{
+
+    [SerializeField] private Image[] m_MenuEffects;
+    [SerializeField] private AudioSource m_PunchSound;
+
+    private Image m_EnabledImage;
+
+    [SerializeField] private Animator m_TitleScreenAnimator;
+
+    private float m_CooldownTime = 2;
+    private float m_RandomAnimationTime = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_EnabledImage = m_MenuEffects[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int rn = Random.Range(0, 8);
+        m_CooldownTime -= Time.deltaTime;
+        m_RandomAnimationTime -= Time.deltaTime; 
+
+        if (m_CooldownTime < 0)
+        {
+            if (!m_MenuEffects[rn].IsActive())
+            {
+                m_PunchSound.Play();
+                m_EnabledImage.enabled = false;
+                m_MenuEffects[rn].enabled = true;
+                m_EnabledImage = m_MenuEffects[rn];
+                m_CooldownTime = Random.Range(0.5f, 2);
+            }
+        }
+        if(m_RandomAnimationTime < 0)
+        {
+            m_RandomAnimationTime = 5;
+            m_TitleScreenAnimator.SetTrigger("ShakeT");
+        }
+    }
+}
---
+++ Assets/Scripts/StartStopMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartStopMenu : MonoBehaviour {
+
+    [SerializeField]private Light[] m_Ligts;
+    private bool m_StartLevel = false;
+
+    public void Startlevel()
+    {
+        m_StartLevel = true;
+    }
+    public void StopGame()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (m_StartLevel)
+        {
+            for (int i = 0; i < m_Ligts.Length; i++)
+            {
+                m_Ligts[i].range -= 3 * Time.deltaTime;
+            }
+            if (m_Ligts[1].range < 5)
+                SceneManager.LoadScene("BoxingScene2");
+        }
+    }
+}
