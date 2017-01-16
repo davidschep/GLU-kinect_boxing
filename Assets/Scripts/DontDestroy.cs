8218c777fd3618b9312cf005518e7cf90332a6fe
Storm <stormwitziers123@gmail.com>
2017-01-16 21:09:48 +0100

    Updated Start Menu

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -1,10 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class DontDestroy : MonoBehaviour
 {
+    [SerializeField] private Toggle m_TwoCameraToggle;
+    private bool m_bTwoCameraToggle = false;
+
     void Start()
     {
         DontDestroyOnLoad(gameObject);
     }
+    public void ToggleCameras()
+    {
+        m_bTwoCameraToggle = m_TwoCameraToggle.isOn;
+    }
+
 }
